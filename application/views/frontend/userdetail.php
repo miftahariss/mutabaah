bab489eba9671312b81c6160922f5e45106e92ce
Miftah Aris Setiawan <miftahariss15@gmail.com>
2016-02-21 21:21:03 +0700

    initial commit

--- application/views/frontend/userdetail.php
+++ application/views/frontend/userdetail.php
@@ -18,27 +18,57 @@
                         <thead>
                             <tr>
                                 <th>No</th>
+                                <th>Date</th>
                                 <th>Nama</th>
-                                <th>Email</th>
-                                <th>Gender</th>
-                                <th>Telp</th>
-                                <th>Alamat</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
-                        <?php if ($users > 0) :?>
+                        <?php if ($mutabaah > 0) :?>
                             <?php $i = 1; ?>
-                            <?php foreach($users as $item): ?>
+                            <?php foreach($mutabaah as $item): ?>
                                 <tr class="odd gradeX">
                                     <td><?php echo $i; ?></td>
+                                    <td><?php echo date('Y-m-d', $item['date']); ?></td>
                                     <td><?php echo $item['nama']; ?></td>
-                                    <td><?php echo $item['email']; ?></td>
-                                    <td><?php echo $item['gender']; ?></td>
-                                    <td class="center"><?php echo $item['telp']; ?></td>
-                                    <td class="center"><?php echo $item['alamat']; ?></td>
-                                    <td><a href="<?php echo base_url(); ?>user/detail/<?php echo $item['id']; ?>" class="btn btn-outline btn-primary">Detail</a></td>
+                                    <td><a href="" class="btn btn-outline btn-primary" data-toggle="modal" data-target="#myModal<?php echo $item['user_id']; ?>">Detail</a></td>
                                 </tr>
+                                <!-- Modal POPUP -->
+                                <div class="modal fade" id="myModal<?php echo $item['user_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                            <h4 class="modal-title" id="myModalLabel">Mutabaah <?php echo date('Y-m-d', $item['date']); ?></h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <label>Sholat Jamaah:</label>
+                                            <?php echo $item['sholat_jamaah']; ?><br>
+                                            <label>Shubuh Jamaah:</label>
+                                            <?php
+                                                if($item['shubuh_jamaah'] == 5){
+                                                    echo '<button type="button" class="btn btn-info btn-circle"><i class="fa fa-check"></i></button>';
+                                                } else {
+                                                    echo '<button type="button" class="btn btn-warning btn-circle"><i class="fa fa-times"></i></button>';
+                                                }
+                                            ?><br>
+                                            <label>Sholat Dhuha:</label>
+                                            <?php
+                                                if($item['sholat_dhuha'] == 5){
+                                                    echo '<button type="button" class="btn btn-info btn-circle"><i class="fa fa-check"></i></button>';
+                                                } else {
+                                                    echo '<button type="button" class="btn btn-warning btn-circle"><i class="fa fa-times"></i></button>';
+                                                }
+                                            ?>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                    <!-- /.modal-content -->
+                                </div>
+                                <!-- /.modal-dialog -->
+                            </div>
                                 <?php $i++; ?>
                             <?php endforeach; ?>
                         <?php endif; ?>
